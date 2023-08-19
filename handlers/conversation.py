9b766ec9af8f746f34dd45e7a863bcb27bcdb440
Mohamed Farhaan <farhaan444@gmail.com>
2023-08-19 09:15:59 +0200

    Bug Fix - Fixed controll flow bug in the flight search. - Main menu to be persistant if user does not click on option

--- handlers/conversation.py
+++ handlers/conversation.py
@@ -5,7 +5,7 @@ from telegram.ext import ContextTypes
 from utils.flight_search import get_airports, format_date, is_date_in_past, validate_number, next_step
 
 # KEY BOARD IMPORTS
-from utils.keyboards import airport_menu, flight_type_menu
+from utils.keyboards import airport_menu, flight_type_menu, main_menu
 
 
 async def converstaion(update: Update, context: ContextTypes.DEFAULT_TYPE, **kwargs):
@@ -16,80 +16,85 @@ async def converstaion(update: Update, context: ContextTypes.DEFAULT_TYPE, **kwa
 
     # ------------------------------ FLIGHTS SEARCH ---------------------------------------#
 
-    if context.user_data['flight_type'] != None:
-        for key, value in context.user_data.items():
-            if value == None:
-                if key == 'Departure Airport':
-                    if ':' not in text:
-                        airports_list = get_airports(location=text)
-                        if airports_list == 'Connection Error':
-                            await context.bot.send_message(chat_id=chat_id, text='🤖 Today is not my day! I am having connection issues, please try again later.')
-                            break
-                        elif airports_list == None:
-                            await context.bot.send_message(chat_id=chat_id, text='😕 I cannot find any airport with that city you provided. Please, try again.')
-                            break
+    # Check if data is not empty to allow flight search steps to be carried out
+    if context.user_data != {}:
+        # check if user has clicked on flight type option
+        if context.user_data['flight_type'] != None:
+            for key, value in context.user_data.items():
+                if value == None:
+                    if key == 'Departure Airport':
+                        if ':' not in text:
+                            airports_list = get_airports(location=text)
+                            if airports_list == 'Connection Error':
+                                await context.bot.send_message(chat_id=chat_id, text='🤖 Today is not my day! I am having connection issues, please try again later.')
+                                break
+                            elif airports_list == None:
+                                await context.bot.send_message(chat_id=chat_id, text='😕 I cannot find any airport with that city you provided. Please, try again.')
+                                break
+                            else:
+                                airports_list = airport_menu(
+                                    airports=airports_list)
+                                await context.bot.send_message(chat_id=chat_id, reply_markup=airports_list, text='Please choose a Airport from the list 👇')
+                                break
                         else:
-                            airports_list = airport_menu(
-                                airports=airports_list)
-                            await context.bot.send_message(chat_id=chat_id, reply_markup=airports_list, text='Please choose a Airport from the list 👇')
-                            break
-                    else:
-                        colon_index = text.find(':')
-                        iata = text[colon_index + 1:]
-                        context.user_data[key] = iata.lstrip()
-                        await next_step(update=update, context=context)
-                        break
-
-                if key == 'Destination Airport':
-                    if ':' not in text:
-                        airports_list = get_airports(location=text)
-                        if airports_list == 'Connection Error':
-                            await context.bot.send_message(chat_id=chat_id, text='🤖 Today is not my day! I am having connection issues, please try again later.')
-                            break
-                        elif airports_list == None:
-                            await context.bot.send_message(chat_id=chat_id, text='😕 I cannot find any airport with that city you provided. Please, try again.')
-                            break
-                        else:
-                            airports_list = airport_menu(
-                                airports=airports_list)
-                            await context.bot.send_message(chat_id=chat_id, reply_markup=airports_list, text='Please choose a Airport from the list 👇')
-                            break
-                    else:
-                        colon_index = text.find(':')
-                        iata = text[colon_index + 1:]
-                        context.user_data[key] = iata.lstrip()
-                        await next_step(update=update, context=context)
-                        break
-
-                if key == 'Departure Date (Earliest)' or key == 'Departure Date (Latest)':
-                    validate_date_format = format_date(date=text)
-                    if validate_date_format == None:
-                        error_msg = '😕 Ooops! Your date format is wrong. Please enter date again in this format Day/Month/Year e.g 01/01/2023'
-                        await context.bot.send_message(chat_id=chat_id, text=error_msg)
-                        break
-                    elif is_date_in_past(text) is False:
-                        error_msg = '🤨 You Honestly expect me to search with date that is in the past tense? Please enter a present or future date'
-                        await context.bot.send_message(chat_id=chat_id, text=error_msg)
-                        break
-                    else:
-                        context.user_data[key] = text
-                        await next_step(update=update, context=context)
-                        break
-
-                if key == 'Minimum Lenth Of Stay' or key == 'Maximum Lenth Of Stay' or key == 'How Many Adults':
-                    validate_num = validate_number(number=text)
-                    if validate_num is False:
-                        error_msg = '🫤 Seriously, its not hard. Try again and enter only a number, e.g 2'
-                        await context.bot.send_message(chat_id=chat_id, text=error_msg)
-                        break
-                    else:
-                        if key == 'How Many Adults':
-                            context.user_data[key] = int(text)
+                            colon_index = text.find(':')
+                            iata = text[colon_index + 1:]
+                            context.user_data[key] = iata.lstrip()
                             await next_step(update=update, context=context)
+                            break
+
+                    if key == 'Destination Airport':
+                        if ':' not in text:
+                            airports_list = get_airports(location=text)
+                            if airports_list == 'Connection Error':
+                                await context.bot.send_message(chat_id=chat_id, text='🤖 Today is not my day! I am having connection issues, please try again later.')
+                                break
+                            elif airports_list == None:
+                                await context.bot.send_message(chat_id=chat_id, text='😕 I cannot find any airport with that city you provided. Please, try again.')
+                                break
+                            else:
+                                airports_list = airport_menu(
+                                    airports=airports_list)
+                                await context.bot.send_message(chat_id=chat_id, reply_markup=airports_list, text='Please choose a Airport from the list 👇')
+                                break
+                        else:
+                            colon_index = text.find(':')
+                            iata = text[colon_index + 1:]
+                            context.user_data[key] = iata.lstrip()
+                            await next_step(update=update, context=context)
+                            break
+
+                    if key == 'Departure Date (Earliest)' or key == 'Departure Date (Latest)':
+                        validate_date_format = format_date(date=text)
+                        if validate_date_format == None:
+                            error_msg = '😕 Ooops! Your date format is wrong. Please enter date again in this format Day/Month/Year e.g 01/01/2023'
+                            await context.bot.send_message(chat_id=chat_id, text=error_msg)
+                            break
+                        elif is_date_in_past(text) is False:
+                            error_msg = '🤨 You Honestly expect me to search with date that is in the past tense? Please enter a present or future date'
+                            await context.bot.send_message(chat_id=chat_id, text=error_msg)
                             break
                         else:
                             context.user_data[key] = text
                             await next_step(update=update, context=context)
                             break
+
+                    if key == 'Minimum Lenth Of Stay' or key == 'Maximum Lenth Of Stay' or key == 'How Many Adults':
+                        validate_num = validate_number(number=text)
+                        if validate_num is False:
+                            error_msg = '🫤 Seriously, its not hard. Try again and enter only a number, e.g 2'
+                            await context.bot.send_message(chat_id=chat_id, text=error_msg)
+                            break
+                        else:
+                            if key == 'How Many Adults':
+                                context.user_data[key] = int(text)
+                                await next_step(update=update, context=context)
+                                break
+                            else:
+                                context.user_data[key] = text
+                                await next_step(update=update, context=context)
+                                break
+        else:
+            await context.bot.send_message(chat_id=chat_id, reply_markup=flight_type_menu, text='Please select option 👇')
     else:
-        await context.bot.send_message(chat_id=chat_id, reply_markup=flight_type_menu, text='Please select option 👇')
+        await context.bot.send_message(chat_id=chat_id, reply_markup=main_menu, text='🤖 Please choose an option. 👇')
