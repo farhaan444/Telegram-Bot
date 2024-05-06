6c06721d919763f257369ab9a0707f69fe357a07
Mohamed Farhaan <farhaan444@gmail.com>
2024-05-06 14:14:48 +0200

    logging Enhancements - Added readme file to logging folder - added notification to administors when a except/error occurs

--- handlers/error.py
+++ handlers/error.py
@@ -2,7 +2,10 @@
 
 from telegram import Update
 from telegram.ext import ContextTypes
+from telegram.constants import ParseMode
 import logging
+import datetime
+import config
 
 logger = logging.getLogger(__name__)
 
@@ -10,3 +13,7 @@ logger = logging.getLogger(__name__)
 async def errors(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Error handler that handles bot errors and prints out errors to console """
     logger.exception(context.error)
+    curr_datetime = datetime.datetime.now()
+    reply = f"{curr_datetime} <b>A bot exception/error has occured. Please see logs.</b>"
+    for i in config.ADMINISTRATORS:
+        await context.bot.send_message(chat_id=i, text=reply, parse_mode=ParseMode.HTML)
